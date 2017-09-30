60daa6b55bf8d80441e9ae44e996238b16356092
Sophian Bensaou <sophian@sophian.local>
2017-10-01 04:04:37 +0700

    changed to hh:mm:ss format

--- fastingwithbuddies/ViewController.swift
+++ fastingwithbuddies/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var counter = 0.0
+    var startTimeLabel = "00:00:00"
+    var startTime = TimeInterval()
     var timer = Timer()
     var isPlaying = false
 
@@ -17,8 +18,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        timeLabel.text = String(counter)
-        pauseButton.isEnabled = false
+        timeLabel.text = String(startTimeLabel)
+        stopButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +29,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     
     @IBAction func startTimer(_ sender: AnyObject) {
@@ -37,15 +38,17 @@ class ViewController: UIViewController {
         }
         
         startButton.isEnabled = false
-        pauseButton.isEnabled = true
+        stopButton.isEnabled = true
         
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
         isPlaying = true
+        
+        startTime = Date.timeIntervalSinceReferenceDate
     }
     
-    @IBAction func pauseTimer(_ sender: AnyObject) {
+    @IBAction func stopTimer(_ sender: AnyObject) {
         startButton.isEnabled = true
-        pauseButton.isEnabled = false
+        stopButton.isEnabled = false
         
         timer.invalidate()
         isPlaying = false
@@ -53,17 +56,38 @@ class ViewController: UIViewController {
     
     @IBAction func resetTimer(_ sender: AnyObject) {
         startButton.isEnabled = true
-        pauseButton.isEnabled = false
+        stopButton.isEnabled = false
         
         timer.invalidate()
         isPlaying = false
-        counter = 0.0
-        timeLabel.text = String(counter)
+        timeLabel.text = String(startTimeLabel)
     }
     
     @objc func UpdateTimer() {
-        counter = counter + 0.1
-        timeLabel.text = String(format: "%.1f", counter)
+        let currentTime = Date.timeIntervalSinceReferenceDate
+        
+        // Find the difference between current time and start time.
+        var elapsedTime: TimeInterval = currentTime - startTime
+        
+        // Calculate the hours in elapsed time.
+        let hours = UInt8(elapsedTime / 3600.0)
+        elapsedTime -= (TimeInterval(hours) * 3600)
+        
+        // Calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (TimeInterval(minutes) * 60)
+        
+        // Calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= TimeInterval(seconds)
+        
+        // Add the leading zero for hours, minutes and seconds and store them as string constants
+        let strHours = String(format: "%02d", hours)
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        
+        // Concatenate hours, minutes and seconds and assign it
+        timeLabel.text = "\(strHours):\(strMinutes):\(strSeconds)"
     }
 }
 
